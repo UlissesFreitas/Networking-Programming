03d9fed119bf84af9a6fc1e58acaa34dc9aeac51
ulisses.filho <ulisses.filho@escolar.ifrn.edu.br>
2024-07-19 23:34:34 +0000

    update

--- File-Server/servidor/servidor.py
+++ File-Server/servidor/servidor.py
@@ -1,6 +1,10 @@
 import os
 import socket
 
+HOST = '127.0.0.1'
+PORT = 12345
+
+# Lista os arquivos disponíveis no diretório local
 def listar_arquivos():
     # Obtendo o diretório corrente
     strDiretorio = os.path.abspath(__file__)
@@ -14,3 +18,46 @@ def listar_arquivos():
 
     print('\n')
 
+
+# Verifica se o arquivo existe no diretório local   
+def VerificaArquivo(fileName):
+    strDiretorio = os.path.abspath(__file__)
+    strDiretorio = os.path.dirname(strDiretorio)
+    strDiretorio = strDiretorio + '/arquivos/'
+
+    if not os.path.exists(strDiretorio + fileName):
+        return False
+    else:
+        return True
+    
+# Cria o socket 
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket.connect((HOST, PORT))
+
+
+while True:
+
+    # Lista os arquivos disponíveis no diretório local    
+    listar_arquivos()
+
+    # Recebe o nome do arquivo que o cliente deseja baixar
+    data, source = socket.recvfrom(1024)
+    fileName =data.decode('utf-8')
+    if not fileName:
+        break
+
+
+    
+    # Verifica se o arquivo existe no diretório local
+    if VerificaArquivo(fileName):
+        # Envia o arquivo para o cliente
+        with open(strDiretorio + fileName, 'rb') as file:
+            data = file.read(1024)
+            while data:
+                socket.sendall(data)
+                data = file.read(1024)
+        print('Arquivo enviado com sucesso!')
+    else:
+        print('Arquivo não encontrado!')
+
+socket.close()
