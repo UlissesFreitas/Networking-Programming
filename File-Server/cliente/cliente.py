95b1ddd8ac8c6d0b8069d71ea57851ce614bdc0a
ulisses.filho <ulisses.filho@escolar.ifrn.edu.br>
2024-07-27 03:00:42 +0000

    update

--- File-Server/cliente/cliente.py
+++ File-Server/cliente/cliente.py
@@ -2,20 +2,48 @@
 import socket
 
 HOST = '10.88.0.4' #Definindo o IP do servidor
-PORT = 50000 #Definindo a porta
+PORT = 50004 #Definindo a porta
+MAX_SIZE = 4294967296
+
+# status de erro interno do servidor
+STATUS_ISE = '500'
+
+# status 404 arquivo nao encontrado
+STATUS_NOT_FOUND = '404'
+
+# status 400 requisicao invalida
+STATUS_BAD = '400'
+
+# status 302 arquivo encontrado
+STATUS_FOUND  = '302'
+
+# status 200 ok arquivo enviado
+STATUS_OK = '200'
+
+
 
 # Criando o socket UDP
 tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 tcp_socket.connect((HOST, PORT)) # Ligando o socket a porta
 
-msg = input('Digite a mensagem: ')
+
+while True:
+    cmd = input('Digite a mensagem: ')
 
 # Convertendo a mensagem digitada de string para bytes
-msg = msg.encode('utf-8')
+    cmd = cmd.encode('utf-8')
 
-# Enviando a mensagem ao servidor
-tcp_socket.send(msg)
+# analisa se o comando e valido
+
+
+# Enviando o comando ao servidor
+    tcp_socket.send(cmd)
+
+# Espera o codigo de status enviado pelo servidor
+
+
+    print(status.decode('utf-8'))
 
 # Fechando o socket
 tcp_socket.close()
--- File-Server/servidor/servidor.py
+++ File-Server/servidor/servidor.py
@@ -1,24 +1,25 @@
 # Importando a biblioteca socket
 import socket
+import os 
 
 HOST = '' #Definindo o IP do servidor
-PORT = 50000 #Definindo a porta
+PORT = 50004 #Definindo a porta
 MAX_SIZE = 4294967296
 
 # status de erro interno do servidor
-STATUS_ISE = 500
+STATUS_ISE = '500'
 
 # status 404 arquivo nao encontrado
-STATUS_NOT_FOUND = 404
+STATUS_NOT_FOUND = '404'
 
 # status 400 requisicao invalida
-STATUS_BAD = 400
+STATUS_BAD = '400'
 
 # status 302 arquivo encontrado
-STATUS_FOUND  = 302
+STATUS_FOUND  = '302'
 
 # status 200 ok arquivo enviado
-STATUS_OK = 200
+STATUS_OK = '200'
 
 # Retorna uma lista com os arquivos disponiveis na pasta de 
 # Funcao 100% pronta
@@ -28,8 +29,6 @@ def listar_arquivos():
     strDiretorio = os.path.dirname(strDiretorio)
     strDiretorio = strDiretorio + '/arquivos/'
 
-
-    #for i in os.listdir(strDiretorio):
     #   print('\033[32m' +  i + '\033[0;0m' )
 
     return os.listdir(strDiretorio)
@@ -39,17 +38,26 @@ def listar_arquivos():
 # Funcao 80% pronta, falta retirar os caracteres especiais
 # Vitoria
 
-def VerificaArquivo(fileName):
+def retirando_caracteres(arquivo):
+    permitidos="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_-."
+    filename=''.join(letra for letra in arquivo if letra in permitidos)
+    return filename
+
+def VerificaNomeArquivo(filename):
     strDiretorio = os.path.abspath(__file__)
     strDiretorio = os.path.dirname(strDiretorio)
     strDiretorio = strDiretorio + '/arquivos/'
 
-    if not os.path.exists(strDiretorio + fileName):
+    if not filename: return False
+    if not os.path.exists(strDiretorio + filename):
         return False
     
     return True
 
-
+def VerificaComando(comando):
+    # verifica se o comando começa com GET/
+    # vitoria
+    pass
 
 # Criando o socket TCP
 tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -58,13 +66,42 @@ tcp_socket.listen(1) # Máximo de conexões enfileiradas
 print('Recebendo Mensagens...\n\n')
 
 while True:
+    print('Esperando uma nova conexao')
     con, cliente = tcp_socket.accept() # Aceita a conexão com o cliente
     print('Conectado por: ', cliente)
+    
+    
     while True:
-        msg = con.recv(1024) #buffer de 1024 bytes
-        if not msg: break
-        # Imprimindo a mensagem recebida convertendo de bytes para string
-        print(cliente, msg.decode('utf-8'))
-        print('Finalizando Conexão do Cliente ', cliente)
+        # Espera a primeira mensagem
+        # deve ser um comando para listar, enviar ou sair
+        cmd = con.recv(10)
 
-    con.close()
+        if not cmd: continue
+
+
+
+        print('.')
+        msg = con.recv(1024) #buffer de 10 bytes
+        if not msg: continue
+        # a msg que deve ser um comando no formato
+        # GET/arquivo.txt
+        # GET/boneco.jpeg
+        # analisar para saber se o comando e valido
+        # se nao contiver 'GET/' em msg o comando sera consideraddo invalido
+        # caso o comando seja invalido deve enviar um codigo de erro ao cliente
+        # e depois voltar ao inicio do while
+        # vitoria
+
+        # --------------------------
+        # Analisa se o nome do arquivo e valido, se o arquivo existe
+        # caso o arquivo nao exista retorna um codigo de erro ao cliente
+
+
+        # Imprimindo a mensagem recebida convertendo de bytes para string
+        print(cliente, msg)
+        
+
+
+
+# THE END
+con.close()
